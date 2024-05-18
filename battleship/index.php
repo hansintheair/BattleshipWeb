66c3c0dcdeac7fa8cd56af586224d45c733070ef
hansintheair <40738670+hansintheair@users.noreply.github.com>
2024-05-17 18:52:40 -0700

    initial commit (#1)

---
+++ battleship/index.php
@@ -0,0 +1,20 @@
+
+<html>
+    <head>
+    
+        <h1>Battleship</h1>
+    
+    </head>
+
+    <body>
+
+    </body>
+</html>
+
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
