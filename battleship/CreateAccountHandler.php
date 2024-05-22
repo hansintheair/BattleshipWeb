787dbacc7edf831f5361a342cd2d556142174545
MatthewR <matt.rdgez2@outlook.com>
2024-05-21 21:03:36 -0700

    regex fix on email and password

--- battleship/CreateAccountHandler.php
+++ battleship/CreateAccountHandler.php
@@ -10,6 +10,21 @@ $store_db = new BattleshipsDB();
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
 
+$pattern = '/^[^@.]+(?:\.[^@.]+)*@[^@.]+\.[^@.]{2,}(?:\.[^@.]{2,})*$/';
+$passwordpattern = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$/';
+
+if (!preg_match($pattern, $email)) {
+    $_SESSION["register_error"] = "Invalid email format";
+    header("Location: CreateAccount.php");
+    exit;
+}
+
+if (!preg_match($passwordpattern, $password)) {
+    $_SESSION["register_error"] = "Password must contain at least one lowercase letter, one uppercase letter, and one number";
+    header("Location: CreateAccount.php");
+    exit;
+}
+
 //echo "PASSWORD: ".$password."<br>";  //DEBUG
 //echo "EMAIL: ".$email."<br>";  //DEBUG
 
@@ -19,13 +34,6 @@ $exists = $store_db->checkUserExists($email);
 $store_db->disconnect();
 if ($exists) {
     $_SESSION["register_error"] = "A user with that e-mail already exists";
-    header("Location: CreateAccount.php");
-    exit;
-}
-
-// Validate password
-if (!$password) {  //TODO: Could use some extra validation
-    $_SESSION["register_error"] = "Bad password";
     header("Location: CreateAccount.php");
     exit;
 }
